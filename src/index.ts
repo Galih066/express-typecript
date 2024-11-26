b61bd7ab376d40417060f6ac1f0c7a11d2138410
Galih Dapa Imanda <galihdapaimanda@gmail.com>
2024-11-26 14:22:26 +0800

    fix(Index): allow CORS

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    res.status(200).json({
+        success: true,
+        message: "Nodejs Express Typescript Boilerplate",
+        path: req.originalUrl
+    });
+});
+
+// Handle 404 - Not Found
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        message: "Not Found - The requested resource does not exist",
+        path: req.originalUrl
+    });
 });
 
 app.listen(port, () => {
